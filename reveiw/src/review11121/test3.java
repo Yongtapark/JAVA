bb2cf661ed807037d1448f1235fa35eb43c5c0ff
Yongtapark <91367204+Yongtapark@users.noreply.github.com>
2022-11-21 23:35:09 +0900

    test

---
+++ reveiw/src/review11121/test3.java
@@ -0,0 +1,45 @@
+package review11121;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class test3 extends JFrame {
+	JPanel jp=new JPanel();
+	JLabel cell;
+	
+	test3(){
+		setTitle("test3");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(4,4));
+		
+		
+		
+		for(int i=0; i<16;i++) {
+			cell =new JLabel(Integer.toString(i));
+			cell.setOpaque(true);
+			add(cell);
+			int r= (int)(Math.random()*256);
+			int g= (int)(Math.random()*256);
+			int b= (int)(Math.random()*256);
+			cell.setBackground(new Color(r,g,b));
+			
+		}
+		
+		setSize(500,500);
+		setVisible(true);
+		//jp.requestFocus();
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new test3();
+
+	}
+
+}
